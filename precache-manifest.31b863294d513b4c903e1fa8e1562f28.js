8e7bb138f7f50c08890cea18fba996dc68a37c9d
ZdraviiSmisl <gormoshka123@gmail.com>
2020-02-09 14:54:45 +0300

    Updates

---
+++ precache-manifest.31b863294d513b4c903e1fa8e1562f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee71a237e31d9aba0530b9faf3cb7ce2",
+    "url": "/socialnetwork/index.html"
+  },
+  {
+    "revision": "2557708430ea91d46dd7",
+    "url": "/socialnetwork/static/css/main.b247f686.chunk.css"
+  },
+  {
+    "revision": "45eea3fde45ab6bc64db",
+    "url": "/socialnetwork/static/js/2.868a7841.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/socialnetwork/static/js/2.868a7841.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2557708430ea91d46dd7",
+    "url": "/socialnetwork/static/js/main.d81dca8d.chunk.js"
+  },
+  {
+    "revision": "85b00f46d54ea291eb65",
+    "url": "/socialnetwork/static/js/runtime-main.ad537159.js"
+  }
+]);
