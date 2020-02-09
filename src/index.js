44fc35e623ef6dfdb2234e5506182ffd768f04bf
ZdraviiSmisl <gormoshka123@gmail.com>
2020-02-09 16:31:21 +0300

    апрапр

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 const rerenderEntireTree = (state) => {
     ReactDOM.render(
-        <BrowserRouter>
+        <HashRouter>
            <Provider store={store}>
             <App />
        </Provider>
-        </BrowserRouter>, document.getElementById('root'));
+        </HashRouter>, document.getElementById('root'));
 }
 
 
