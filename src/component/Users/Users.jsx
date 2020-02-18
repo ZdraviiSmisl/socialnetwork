84b4372145e09a273b1d35e7b91c9ccaf9014d4c
ZdraviiSmisl <gormoshka123@gmail.com>
2020-02-18 18:46:14 +0300

    new commit

--- src/component/Users/Users.jsx
+++ src/component/Users/Users.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import UsersStyles from './users.module.css'
+import  * as  axios from 'axios';
+import userPhoto from '../../../src/StandardAvatar/images/StanadartUser.jpg';
 
 const Users = (props) => {
     debugger;
     if(props.users.length===0){
-        props.setUsers(
-
-            [{id:1,fullName:'Jeca',Status:'many is power',location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:true},
-                {id:2,fullName:'Kolian',Status:'i agree with you',location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:false},
-                {id:3,fullName:'Ivan',Status:"no,it's only paper",location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:true},
-        ])
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response=>{
+            debugger;
+            props.setUsers(response.data.items);
+        });
 
     }
 
@@ -20,7 +20,7 @@ const Users = (props) => {
             props.users.map(u => <div key={u.id}>
             <span>
 
-                <div><img src={u.photoUrl} className={UsersStyles.userPhoto}/></div>
+                <div><img src={u.photos.small != null?u.photos.small: userPhoto } className={UsersStyles.userPhoto}/></div>
                 <div>{u.followed ? <button onClick={() => {
                     props.unfollow(u.id)
                 }}>Unfollow</button> : <button onClick={() => {
@@ -30,13 +30,13 @@ const Users = (props) => {
 
                 <span>
                   <span>
-                      <div>{u.fullName}</div>
+                      <div>{u.name}</div>
                   <div>{u.Status}</div>
 
                   </span>
                   <span>
-                      <div>{u.location.contry}</div>
-                      <div>{u.location.city}</div>
+                      <div>{'u.location.contry'}</div>
+                      <div>{'u.location.city'}</div>
 
                   </span>
 
@@ -51,3 +51,11 @@ const Users = (props) => {
 
 
 export default Users;
+
+
+/*
+
+[{id:1,fullName:'Jeca',Status:'many is power',location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:true},
+    {id:2,fullName:'Kolian',Status:'i agree with you',location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:false},
+    {id:3,fullName:'Ivan',Status:"no,it's only paper",location:{contry:'Russia',city:'Pushkino'},photoUrl: 'https://avatarko.ru/img/kartinka/33/film_muzhchina_ulybka_32755.jpg',followed:true},
+])*/
