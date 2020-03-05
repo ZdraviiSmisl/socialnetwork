8689a8a5400598453bc2a17e6e390936df358684
Evgeniy <gormoshka123@gmail.com>
2020-03-05 13:36:25 +0300

    2

--- src/component/Profile/ProfileContainer.jsx
+++ src/component/Profile/ProfileContainer.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import Profile from './Profile';
 import * as axios from "axios";
 import {connect} from "react-redux";
-import MyPosts from "./MyPosts/MyPosts";
 import {setUserProfile} from "../../redux/profile-reducer";
 import {withRouter} from "react-router-dom";
 
 class ProfileContainer extends React.Component {
     componentDidMount() {
-        let userId = this.props.match.params.userId; //получаем наш юзер айди и записываем  в  строку ниже  в запрос вместо цифры2,match-совпадение нашего урла с роутами
+        let userId = this.props.match.params.userId; /*//получаем наш юзер айди и записываем  в  строку ниже  в запрос вместо цифры2,match-совпадение нашего урла с роутами*/
         if (!userId) {
-            userId = 2; // если параметра юзер айди  нет в урле .. то по умолчанию ставим цифру 2
+            userId = 2; /*// если параметра юзер айди  нет в урле .. то по умолчанию ставим цифру 2*/
         }
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId) //либо  если в конце с помощтю конкатинации добавить через плюс
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId) /*//либо  если в конце с помощтю конкатинации добавить через плюс*/
             .then(response => {
-                this.props.setUserProfile(response.data); //берём тут profile для AC
+                this.props.setUserProfile(response.data); /*//берём тут profile для AC*/
             });
     }
 
@@ -22,8 +21,8 @@ class ProfileContainer extends React.Component {
     render() {
         return (
             <div>
-                <Profile {...this.props} profile={this.props.profile}/> //передаём помимо пропсов извне(если они придут)
-                ещё и profile с сервера
+                <Profile {...this.props} profile={this.props.profile}/> {/*//передаём помимо пропсов извне(если они придут)*/}
+               {/* ещё и profile с сервера*/}
 
 
             </div>
@@ -32,7 +31,7 @@ class ProfileContainer extends React.Component {
 }
 
 let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile  //передаём это в Profile
+    profile: state.profilePage.profile  /*//передаём это в Profile*/
 
 })
 let WithUrlDataContainerComponet = withRouter(ProfileContainer);/*// оборачиваем в контейнер withrouter*/
